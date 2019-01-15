2b7984cfb8682dcfdc99666e5027935eeafed31f
CHENG-KUN <chengkunche@yahoo.com>
2019-01-15 14:28:14 -0500

    database

---
+++ database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"navigator-api/config"
+	"navigator-api/logs"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+var DB *gorm.DB
+
+// Opening a database and save the reference to `Database` struct.
+func Init() *gorm.DB {
+
+	dbSource := config.GetConfig().DbUsername + ":" + config.GetConfig().DbPwd + "@tcp(" + config.GetConfig().DbUrlAndPort + ")/" + config.GetConfig().DbSchemaName + "?" + config.GetConfig().DbArgs
+	db, err := gorm.Open("mysql", dbSource)
+	if err != nil {
+		logs.GetLogger().Fatal("db err: ", err)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(10)
+	db.LogMode(false)
+	DB = db
+	return DB
+}
+
+// Using this function to get a connection, you can create your connection pool here.
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func SaveOne(data interface{}) error {
+	db := GetDB()
+	err := db.Save(data).Error
+	return err
+}
